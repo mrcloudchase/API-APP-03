c6ee1586b1900322fdf9a96c64972998ed2ab7bd
mrcloudchase <mrcloudchase@gmail.com>
2024-05-13 19:36:32 -0500

    add dev branch

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/scripteditor', methods=['GET'])
 def get_endpoint_2():
     endpoint_name = request.endpoint
-    return jsonify({"endpoint": endpoint_name})
+    return jsonify({"endpoint": endpoint_name, "branch": "uat"})
 
 if __name__ == '__main__':
     app.run(debug=True)
